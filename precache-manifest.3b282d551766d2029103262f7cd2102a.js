80c5eb0b5bbde59640e0c472208e408fba1c2446
Tavaru5 <silas13579@gmail.com>
2020-08-15 19:00:42 -0400

    Updates

---
+++ precache-manifest.3b282d551766d2029103262f7cd2102a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6db1246a56a9ad62bb6b8fd33ba9b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9727d1f5b6e426a9576",
+    "url": "/static/css/main.6e3e1da4.chunk.css"
+  },
+  {
+    "revision": "e0b144b1baf1b3ea8767",
+    "url": "/static/js/2.c6a2f438.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c6a2f438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9727d1f5b6e426a9576",
+    "url": "/static/js/main.7546b439.chunk.js"
+  },
+  {
+    "revision": "db1bf0d891ca5c449f2e",
+    "url": "/static/js/runtime-main.123dd0a8.js"
+  },
+  {
+    "revision": "542bab62e584a2fa275817fd67efd77f",
+    "url": "/static/media/aboutMeIcon.542bab62.png"
+  },
+  {
+    "revision": "1be8fa3117f9c512e65a33f3726510d3",
+    "url": "/static/media/githubIcon.1be8fa31.png"
+  },
+  {
+    "revision": "2118ebde61a8f21646477f2c2c17d24c",
+    "url": "/static/media/playAGameIcon.2118ebde.png"
+  },
+  {
+    "revision": "a2344c4767e50c3eabccd0ba2624abc1",
+    "url": "/static/media/portfolioIcon.a2344c47.png"
+  },
+  {
+    "revision": "e3a6ead3de2305ac0b32576c643e8778",
+    "url": "/static/media/restart.e3a6ead3.svg"
+  },
+  {
+    "revision": "c4933a3f3583936c357e4ee3fc192c35",
+    "url": "/static/media/twitterIcon.c4933a3f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2979b11ce8606b19b8b79b0ed4bce8c1.js"
+  "/precache-manifest.3b282d551766d2029103262f7cd2102a.js"
 );
 
 self.addEventListener('message', (event) => {
