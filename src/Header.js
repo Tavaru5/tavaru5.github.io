cf4e642ffc88951a79e4976a76e044a6cf4f56a9
Tavaru5 <silas13579@gmail.com>
2021-10-31 15:40:07 -0400

    Actually commit the header

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Spring } from "react-spring/renderprops";
+import { Link } from "react-router-dom";
+
+export default function Header(props) {
+  return (
+    <Spring from={{ opacity: 0 }} to={{ opacity: props.endOpacity }}>
+      {(opacityAnim) => (
+        <Link
+          style={{
+            textDecoration: "none",
+            color: "#000",
+            position: "absolute",
+            bottom: "49vw",
+            left: "9.5vw",
+            ...opacityAnim,
+            ...props.style,
+          }}
+          to="/"
+        >
+          HOME
+        </Link>
+      )}
+    </Spring>
+  );
+}
